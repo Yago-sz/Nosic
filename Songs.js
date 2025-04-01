b109161b90c538fac885bbd56101933c4d8b00be
Yago <yagosantoslima81@gmail.com>
2025-04-01 19:25:06 -0300

    alo

--- Songs.js
+++ Songs.js
@@ -1,24 +1,19 @@
 export default [
+    
+  {
+    src: "./musicas/devil.mp3", // ⬅ Nome corrigido!
+    name: "Evanescence - Afterlife",
+    cover: "./capas/devil.jpg",
+  },
     {
       src: "./musicas/Faroeste Caboclo.mp3", // ⬅ Nome corrigido!
       name: "Faroeste Caboclo",
       cover: "./capas/faroeste.jpg",
-    
     },
     {
         src: "./musicas/Black.mp3", // ⬅ Nome corrigido!
         name: "Black",
         cover: "./capas/black.jpg",
-      },
-      {
-        src: "./musicas/run.mp3", // ⬅ Nome corrigido!
-        name: "Gyro Zeppeli",
-        cover: "./capas/gyro.jpg",
-      },
-      {
-        src: "./musicas/arthur.mp3", // ⬅ Nome corrigido!
-        name: "Olhos Sempre Abertos",
-        cover: "./capas/olhos.jpg",
       },
       {
         src: "./musicas/jojo.mp3", // ⬅ Nome corrigido!
@@ -28,15 +23,18 @@ export default [
       {
         src: "./musicas/bah.mp3", // ⬅ Nome corrigido!
         name: "Labrinth - I'm Tired ",
-        cover: "./capas/céu.jpeg",
+        cover: "./capas/céu.jpg",
+      },
+      {
+        src: "./musicas/holy.mp3", // ⬅ Nome corrigido!
+        name: "Dio - Holy Diver ",
+        cover: "./capas/dio.jpg",
       },
 
-      {
-        src: "./musicas/diego.mp3", // ⬅ Nome corrigido!
-        name: "♪ Diego Brando ｜ Jurassic World ｜ AniRap",
-        cover: "./capas/diego.jpg",
-      },
       
+    
       
+
+     
       
 ];
